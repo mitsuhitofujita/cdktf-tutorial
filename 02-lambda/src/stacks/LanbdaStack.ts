d7fdb26f62294cccfdfb332cf118322da6d8b3bc
Mitsuhito Fujita <mitsuhitofujita@gmail.com>
2023-03-11 02:27:40 +0000

    Refactor lambda settings

--- 02-lambda/src/stacks/LanbdaStack.ts
+++ 02-lambda/src/stacks/LanbdaStack.ts
@@ -1,8 +1,8 @@
 import { Construct } from "constructs";
-import { AssetType, S3Backend, TerraformAsset, TerraformOutput, TerraformStack } from "cdktf";
+import { S3Backend, TerraformOutput, TerraformStack } from "cdktf";
 import * as aws from "@cdktf/provider-aws";
-import path = require("path");
 import { createPrivateS3Bucket } from "../provider/private-s3-bucket";
+import { createHttpLambdaFunction } from "../provider/http-lambda-function";
 
 export interface LambdaStackConfig {
   backend: {
@@ -28,100 +28,27 @@ export class LambdaStack extends TerraformStack {
 
     new aws.provider.AwsProvider(this, 'provider', config.provider);
 
-    const bucket = createPrivateS3Bucket(this, 'lambda', {
+    const lambdaBucket = createPrivateS3Bucket(this, 'lambda', {
       prefix,
       environment: config.environment,
     })
 
-    const asset = new TerraformAsset(this, 'asset', {
-      path: path.resolve(__dirname, '../../../lambda/hello/dist'),
-      type: AssetType.ARCHIVE,
-    });
-
-    const archive = new aws.s3Object.S3Object(this, 'archive', {
-      bucket: bucket.bucket,
-      key: `hello/${asset.assetHash}/${asset.fileName}`,
-      source: asset.path,
-      etag: asset.assetHash,
-    });
-
-    const lambdaAssumeRolePolicy = {
-      "Version": "2012-10-17",
-      "Statement": [
-        {
-          "Action": "sts:AssumeRole",
-          "Principal": {
-            "Service": "lambda.amazonaws.com"
-          },
-          "Effect": "Allow",
-          "Sid": ""
-        }
-      ]
-    };
-
-    const role = new aws.iamRole.IamRole(this, 'role', {
-      name: `${prefix}-${config.environment}`,
-      assumeRolePolicy: JSON.stringify(lambdaAssumeRolePolicy),
-    });
-
-    const lambdaLoggingPolicy = {
-      "Version": "2012-10-17",
-      "Statement": [
-        {
-          "Effect": "Allow",
-          "Action": [
-            "logs:CreateLogStream",
-            "logs:PutLogEvents",
-            "cloudwatch:PutMetricData",
-          ],
-          "Resource": [
-            "arn:aws:logs:*:*:*"
-          ]
-        }
-      ]
-    };
-
-    const loggingPolicy = new aws.iamPolicy.IamPolicy(this, 'logging-policy', {
-      name: `${prefix}-logging-${config.environment}`,
-      policy: JSON.stringify(lambdaLoggingPolicy),
-    })
-
-    const loggingPolicyAttachment = new aws.iamRolePolicyAttachment.IamRolePolicyAttachment(this, 'logging-policy-attachment', {
-      policyArn: loggingPolicy.arn,
-      role: role.name
-    });
-
-    const logGroup = new aws.cloudwatchLogGroup.CloudwatchLogGroup(this, 'log-group', {
-      name: `/aws/lambda/${prefix}-${config.environment}`,
-      retentionInDays: 3,
-      skipDestroy: false,
-    });
-
-    const lambdaFunction = new aws.lambdaFunction.LambdaFunction(this, 'function', {
-      functionName: `${prefix}-${config.environment}`,
-      s3Bucket: bucket.bucket,
-      s3Key: archive.key,
-      sourceCodeHash: asset.assetHash,
+    const helloLambda = createHttpLambdaFunction(this, 'hello_lambda', {
+      assetPath: 'lambda/hello/dist',
+      bucket: lambdaBucket,
+      environment: config.environment,
+      lambdaName: 'hello',
+      prefix,
       handler: 'lambda.handler',
       runtime: 'nodejs16.x',
-      role: role.arn,
-      dependsOn: [
-        logGroup,
-        loggingPolicyAttachment,
-      ]
-    });
-
-    const functionUrl = new aws.lambdaFunctionUrl.LambdaFunctionUrl(this, 'function-url', {
-      functionName: lambdaFunction.functionName,
-      authorizationType: 'NONE',
-    });
+    })
 
     new TerraformOutput(this, 'output-function-arn', {
-      value: lambdaFunction.arn,
+      value: helloLambda.function.arn,
     });
 
     new TerraformOutput(this, 'output-function-url', {
-      value: functionUrl.functionUrl,
+      value: helloLambda.functionUrl.functionUrl,
     });
   }
 }
